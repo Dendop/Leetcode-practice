01fd7cf5c3890fd43021522c6960ddace9abd027
Dendop <denopod@gmail.com>
2024-09-19 19:44:04 +0100

    Update rem_dup.py

--- rem_dup.py
+++ rem_dup.py
@@ -1,1 +1,15 @@
-
+def main():
+    
+    nums = [1, 1, 2, 5, 5, 6]
+    j = 1
+    for i in range(1, len(nums)):
+        if nums[i] != nums[i - 1]:
+            nums[j] = nums[i]
+            j += 1
+    
+    print(j)     
+   
+    
+    
+if __name__ == "__main__":
+    main()
