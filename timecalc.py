15f7a6f18942d3d49ecb51462ad9472468c2ce03
Dendop <denopod@gmail.com>
2024-10-05 17:10:34 +0100

    Create timecalc.py

---
+++ timecalc.py
@@ -0,0 +1,16 @@
+import sys
+import re
+
+def add_time(start, duration):
+    
+    
+    
+    
+    return new_time
+
+def main():
+    add_time("3:30 PM", "3:10")
+    
+    
+if __name__ == "__main__":
+    main
