dadc6b0c1786a04e46f90ea7f57930a1d60ef430
Dendop <denopod@gmail.com>
2024-10-09 21:31:24 +0100

    ...

---
+++ mergelists.py
@@ -0,0 +1,18 @@
+
+
+class Node:
+    def __init__(self, data = 0, next = None):
+        self.data = data
+        self.next= next
+        
+
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        
+
+
+def main():
+    
+
+if __name__ == "__main__":
+    main()
