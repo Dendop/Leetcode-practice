fb853a3297626ab3342cfde4b2f5f1f44418ec77
Dendop <denopod@gmail.com>
2024-12-12 20:30:45 +0000

    exercise 2

---
+++ file_handling/email_ex.py
@@ -0,0 +1,23 @@
+import re
+
+def main():
+    
+    email_list = []
+
+    with open("email_exchanges.txt", "r") as file:
+        pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+        
+        
+        for line in file:
+            emails = re.findall(pattern, line)
+            email_list.extend(emails)
+                
+    print(email_list)
+    
+                
+            
+    
+    
+    
+if __name__ == "__main__":
+    main()
