df7abb2e905b724c428a31a2d934c8a1a60b05ea
Dendop <denopod@gmail.com>
2024-12-16 13:15:58 +0000

    email extraction

--- file_handling/email_ex.py
+++ file_handling/email_ex.py
@@ -4,16 +4,20 @@ def main():
     
     email_list = []
 
-    with open("email_exchanges.txt", "r") as file:
-        pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
+    try:
+        with open("email_exchanges.txt", "r") as file:
+            pattern = r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}"
         
         
-        for line in file:
-            emails = re.findall(pattern, line)
-            email_list.extend(emails)
+            for line in file:
+                emails = re.findall(pattern, line)
+                email_list.extend(emails)
                 
-    print(email_list)
+        print(email_list)
     
+    except FileNotFoundError:
+        print("File has not been found")
+        
                 
             
     
