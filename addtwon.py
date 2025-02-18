8438bf72bca78058e1f885f12296ab8bd0ed931a
Dendop <denopod@gmail.com>
2025-02-18 20:54:03 +0000

    leetcode addtwo

---
+++ addtwon.py
@@ -0,0 +1,14 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+def main():
+    l1 = [2, 4, 3]
+    l2 = [5, 6, 4]
+
+
+
+if __name__== "__main__":
+    main()
