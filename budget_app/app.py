a111d2f2254ab6504c3eeb49a910e5fe8f2da6c9
Dendop <denopod@gmail.com>
2025-03-30 21:35:29 +0100

    deposit isn't shared in categories

--- budget_app/app.py
+++ budget_app/app.py
@@ -3,22 +3,28 @@ class Category:
         self.total = 0.0
         self.cat_name = cat_name
         self.ledger = []
-    
+        self.current_total = 0
+    #categories do not share deposit
     def deposit(self,ammount,description = ""):
         self.ammount = ammount
         self.total += ammount
         self.description = description
-        self.ledger.append({"ammount":ammount, "description":description})
+        self.ledger.append({"ammount": float(f"{ammount:.2f}"), "description":description})
+        
     
-    def withdraw(self, ammount, description):
-        self.ammount = ammount
-        self.total -= ammount
+    def withdraw(self, ammount, description = ""):
         if self.total < 0:
             return False
-        self.ledger.append({"ammount": ammount, "description": description})
+        self.ammount = ammount
+        self.total -= ammount
+        self.description = description
+        self.ledger.append({"ammount": -ammount, "description": description})
         
     def __str__(self):
-        return f"*************{self.cat_name}************\n{self.ledger}\n{self.total}"
+        #some_string = ""
+        #for i in self.ledger:
+        return f"{self.cat_name}\n{self.ledger}\n{self.total:.2f}"
+    
 def create_spend_chart(categories):
     pass
 
@@ -26,8 +32,10 @@ def main():
     food = Category("Food")
     food.deposit(500, 'deposit')
     food.withdraw(100, "babanas")
+    clothing = Category("Clothing")
+    clothing.withdraw(150, 'new t-shirt')
     print(food)
-
+    print(clothing)
 
 
 
