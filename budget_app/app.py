d00a00dd124aed744190696b82e9c9d2fb794c96
Dendop <denopod@gmail.com>
2025-04-09 11:56:34 +0100

    budget app update, chart

--- budget_app/app.py
+++ budget_app/app.py
@@ -45,7 +45,44 @@ class Category:
         return title + items + total_line
     
 def create_spend_chart(categories):
-    title = "Percentage spent by category"
+    #calculate spent for each category
+    spent_category = []
+    for cat in categories:
+        spent = 0
+        for entry in cat.ledger:
+            if entry['amount'] < 0: #this checks if it's - value
+                spent += -entry['amount']
+        spent_category.append(spent) #add the value into list
+    total_spent = sum(spent_category) #sums all values in list
+    
+    #calculate percentage
+    percentage = []
+    perc = 0
+    for each in spent_category:
+        perc = int((each / total_spent) * 100) // 10 * 10 #if 33% 33 // 10 = 3 * 10 = 30, rounding
+        percentage.append(perc)
+    
+    chart = "Percentage spent by category\n"
+    for i in range(100, -1, -10): #start, stop, steps
+        line = f"{i:>3}|" #3 characters aligned to right
+        for percent in percentage:
+            line += " o " if percent >= i else "   " #if percent is equal or higher then o else empty string
+        line += "  "
+        chart += line + "\n"
+
+    chart += "    " + "-" * (3 * len(categories) + 1) + "\n"
+
+    max_len = max(len(c.cat_name) for c in categories)
+    for i in range(max_len):
+        line = "    "
+        for cat in categories:
+            letter = cat.cat_name[i] if i < len(cat.cat_name) else " "
+            line += f" {letter} "
+        line += " "
+        chart += line + "\n"
+
+    return chart.rstrip("\n")
+    
 
 def main():
     food = Category("Food")
@@ -55,7 +92,8 @@ def main():
     clothing.deposit(400, 'deposit')
     clothing.withdraw(350, 'new t-shirt')
     food.transfer(50,clothing)
-    print(food)
+    #print(food)
+    print(create_spend_chart([food,clothing]))
     
     
 
