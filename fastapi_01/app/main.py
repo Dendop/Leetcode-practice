eced9d77b50469905ebdfe38eb1f193fc6ceb6af
Dendop <denopod@gmail.com>
2025-05-09 21:58:32 +0100

    addind get user

--- fastapi_01/app/main.py
+++ fastapi_01/app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
-from typing import Optional
+from typing import Optional, List
 from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import os
 from dotenv import load_dotenv
 import time
-from . import model, schema
+from . import model, schema, utils
 from .database import engine, get_db
 from sqlalchemy.orm import Session
 from sqlalchemy import text # used for raw SQL queries
+
 
 model.Base.metadata.create_all(bind=engine) #initializing db
 
@@ -54,14 +55,14 @@ db_password = os.getenv('DB_PASSWORD')
 def root():
     return {"message": "Hello, World"}
 
-@app.get('/posts')
+@app.get('/posts', response_model=List[schema.ResponsePost])
 def get_posts(db: Session = Depends(get_db)):
     # cursor.execute("""SELECT * FROM "posts" WHERE "id" = 3 """)
     # posts = cursor.fetchall()
     posts = db.query(model.Post).all()
-    return {"data": posts}
+    return posts
 
-@app.post('/posts', status_code=status.HTTP_201_CREATED)
+@app.post('/posts', status_code=status.HTTP_201_CREATED, response_model=schema.ResponsePost)
 def create_posts(post: schema.PostBase, db: Session = Depends(get_db)):
     # cursor.execute("""INSERT INTO "posts" ("title","content","published")
     #                   VALUES (%s, %s, %s)
@@ -75,9 +76,9 @@ def create_posts(post: schema.PostBase, db: Session = Depends(get_db)):
     db.add(new_post)
     db.commit()
     db.refresh(new_post) # return the data to postman or browser(client)
-    return {"data": new_post}
+    return new_post
 
-@app.get('/posts/{id}')
+@app.get('/posts/{id}', response_model=schema.ResponsePost)
 def get_post(id: int, db: Session = Depends(get_db)):
     # cursor.execute(""" SELECT * FROM "posts" WHERE "id" = %s """, (id,))
     # post = cursor.fetchone()
@@ -86,7 +87,7 @@ def get_post(id: int, db: Session = Depends(get_db)):
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
                             detail=f"post with id:{id} was not found")
-    return {"post detail": post}
+    return post
 
 @app.delete('/posts/{id}')
 def delete_post(id: int, db: Session = Depends(get_db)):
@@ -103,9 +104,9 @@ def delete_post(id: int, db: Session = Depends(get_db)):
     
     
     
-    return {"post deleted": post }
+    return post 
 
-@app.put('/posts/{id}')
+@app.put('/posts/{id}', response_model=schema.ResponsePost)
 def update_post(id: int, updated_post: schema.PostBase, db: Session = Depends(get_db)):
     # cursor.execute("""UPDATE "posts" SET "title"= %s, "content"= %s, published= %s WHERE "id" = %s RETURNING *""", 
     #                (post.title, post.content, post.published, (id)))
@@ -120,5 +121,24 @@ def update_post(id: int, updated_post: schema.PostBase, db: Session = Depends(ge
     
     existing_post.update(updated_post.dict(), synchronize_session=False)
     db.commit()
-    return {"Updated post details": existing_post.first()}
+    return existing_post.first()
     
+@app.post('/users', status_code=status.HTTP_201_CREATED, response_model=schema.UserMsg)
+def create_user(user:schema.UserCreate, db: Session = Depends(get_db)):
+    hashed_password = utils.hash(user.password)
+    user.password = hashed_password
+    new_user = model.User(**user.dict()) #Unpacking Pynantic model to create DB model
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+@app.get('/user/{id}', response_model=schema.UserMsg)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(model.User).filter(model.User.id == id).first()
+    
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"User with id:{id} was not found")
+    return user
--- fastapi_01/app/schema.py
+++ fastapi_01/app/schema.py
@@ -1,4 +1,4 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
 class PostBase(BaseModel):
     title : str
@@ -9,3 +9,23 @@ class PostBase(BaseModel):
 class CreatePost(PostBase): #inherits attributes from PostBase
     user : str #this is additional attribute only CreatePost has
     
+    
+class ResponsePost(BaseModel):
+    id: int
+    title: str
+    content: str
+    published: bool = True
+    
+    class Config:
+        orm_mode: True
+        
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+    
+class UserMsg(BaseModel):
+    id: int
+    email: EmailStr
+    
+    class Config:
+        orm_mode = True
