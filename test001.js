ae92bf992678b143fb0ac0e6547b2568a26fc49f
Dendop <denopod@gmail.com>
2025-06-16 21:42:08 +0100

    updts

--- test001.js
+++ test001.js
@@ -1,40 +1,12 @@
-class Media {
-  constructor(title){
-    this._title = title;
-    this._isCheckedOut = false;
-    this._ratings = [];
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
   }
-  get title(){
-    return this._title;
-  }
-  get isCheckedOut(){
-    return this._isCheckedOut;
-  }
-  get ratings(){
-    return this._ratings;
-  }
-  set isCheckedOut(con){
-    this._isCheckedOut = con;
-  }
-  toggleCheckedOutStatus(con) {
-    this._isCheckedOut = !this._isCheckedOut;
-  }
-  getAverageRating() {
-    if (this._ratings.length === 0) return 0;
-    let average = (this._ratings.reduce((acc,curr) => acc + curr)) / this._ratings.length;
-    return `Average: ${average}`;
-  }
-  addRating(num) {
-    this._ratings.push(num);
-  }
-};
+]
 
-const myCD = new Media('Eminen');
-myCD._ratings = [4,3,2,4]
-
-console.log(myCD.getAverageRating())
-console.log(myCD)
-
-myCD.addRating(9);
-console.log(myCD.ratings);
-
+console.log(books[1].author);
--- test010.py
+++ test010.py
@@ -1,15 +1,26 @@
-def main():
-    while counter > 5:
-        sum = 0
-        counter = 1
-        sum += counter
-        counter += 1
-    
-    print(sum)
-    
-    
-    
-    
-    
-if __name__ == "__main__":
-    main()
+1. None means doesn't exist in our system, False is boolean expresion some object or condition can be False but still exist
+   and 0 is integer ValueError
+   
+2. 2,4,6 it doubles the values for all list elements
+3. [lambda x: x ** 2 for x in numbers] lambda is unkown function
+4. pass
+5. 
+    nums = [3, 5, 7, 9, 11]
+    def odd_numbers(arr)
+        odd = []
+        for num in arr:
+            if num % 2 != 0:
+                odd.append(num)
+        return odd
+6. print(my_list[-1]) #not sure what intention were with this
+7.1,4,7,10
+8.  def is_palindrome(str):
+        new_str = str[::-1]
+        if str == new_str:
+            return True
+        else:
+            return False
+9. Lists, boolean, 
+10. syntax error won't run due to interpretation and with runtime error the program ran just hit the problem or something went wrong
+11. probably referencing the variable, try to use different SyntaxError
+12. loop is the function that repeat itself until some rules are met, and condition is some set of rules
